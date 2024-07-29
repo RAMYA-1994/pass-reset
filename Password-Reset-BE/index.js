ee934eb21c7527623321d82fd426db633c29566c
RAMYA-1994 <ramyarrr2016@gmail.com>
2024-07-29 09:28:29 +0530

    update

--- Password-Reset-BE/index.js
+++ Password-Reset-BE/index.js
@@ -1,17 +1,36 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
+import AppRouter from './src/routes/index.js';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-import AppRouter from './src/routes/index.js'
-import cors from 'cors'
+// Load environment variables from .env file
+dotenv.config();
 
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.dbUrl;
 
-dotenv.config()
+// Initialize express app
+const app = express();
 
-const PORT = process.env.PORT
-const app = express()
-app.use(cors())
-app.use(express.json())
+// Middleware
+app.use(cors());
+app.use(express.json());
 
-app.use('/',AppRouter)
+// Routes
+app.use('/', AppRouter);
 
-app.listen(PORT,()=>console.log(`app is listening to port ${PORT}`))
+// Connect to MongoDB
+// mongoose
+//   .connect("mongodb+srv://user1:ramyapraba@cluster0.bnxxvoh.mongodb.net/?retryWrites=true&w=majority&appName=password", { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() => {
+//     console.log('Database Connected...');
+//   })
+//   .catch((error) => {
+//     console.error('NOT CONNECTED! Error:', error.message);
+//   });
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
